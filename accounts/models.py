7170389d49d04bd38e70d1d779c9a2f46c161778
Ayobamidele <ayobamideleewetuga@gmail.com>
2021-10-14 18:36:35 +0100

    Created the user and customer model and registered it in admin

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+	is_customer = models.BooleanField(default=True)
+	is_vendor = models.BooleanField(default=False)
+
+
+class Customer(models.Model):
+	GENDER = (
+			('Male', 'male'),
+			('Female', 'female')
+			)
+	firstname = models.CharField(max_length=200, null=False)
+	lastname = models.CharField(max_length=200, null=False)
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	date_created = models.DateTimeField(auto_now_add=True, null=True)
+	username = models.CharField(max_length=200, null=False)
+	phone_number = models.CharField(max_length=20)
+	email = models.CharField(max_length=200, null=False)
+	profile_pic = models.ImageField(null=True, blank=True)
+	gender = models.CharField(max_length=200, null=True, choices=GENDER)
+	
+	def __str__(self):
+		return str(self.firstname + " " + self.lastname)
