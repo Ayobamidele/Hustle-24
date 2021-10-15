0bcc1af16eef4b290b40580cec82987c2ef7ad65
Ayobamidele <ayobamideleewetuga@gmail.com>
2021-10-15 16:22:54 +0100

    Created the vendor model

--- accounts/models.py
+++ accounts/models.py
@@ -14,13 +14,32 @@ class Customer(models.Model):
 			)
 	firstname = models.CharField(max_length=200, null=False)
 	lastname = models.CharField(max_length=200, null=False)
-	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
 	date_created = models.DateTimeField(auto_now_add=True, null=True)
 	username = models.CharField(max_length=200, null=False)
 	phone_number = models.CharField(max_length=20)
 	email = models.CharField(max_length=200, null=False)
 	profile_pic = models.ImageField(null=True, blank=True)
 	gender = models.CharField(max_length=200, null=True, choices=GENDER)
+	
+	def __str__(self):
+		return str(self.firstname + " " + self.lastname)
+
+class Vendor(models.Model):
+	GENDER = (
+			('Male', 'male'),
+			('Female', 'female')
+			)
+	firstname = models.CharField(max_length=200, null=False)
+	lastname = models.CharField(max_length=200, null=False)
+	user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+	date_created = models.DateTimeField(auto_now_add=True, null=True)
+	username = models.CharField(max_length=200, null=False)
+	phone_number = models.CharField(max_length=20)
+	email = models.CharField(max_length=200, null=False)
+	profile_pic = models.ImageField(null=True, blank=True)
+	gender = models.CharField(max_length=200, null=True, choices=GENDER)
+	is_vendor = models.BooleanField(default=True)
 	
 	def __str__(self):
 		return str(self.firstname + " " + self.lastname)
