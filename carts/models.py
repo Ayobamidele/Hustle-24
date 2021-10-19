9ccb2bfb1ad82a9277af85e8842591dcfa4220bd
Ayobamidele <ayobamideleewetuga@gmail.com>
2021-10-19 13:16:22 +0100

    Created the order model

--- carts/models.py
+++ carts/models.py
@@ -24,3 +24,21 @@ class OrderItem(models.Model):
 
     def __str__(self):
         return self.product.name
+
+class Order(models.Model):
+	ref_code = models.CharField(max_length=15)
+	owner = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
+	is_ordered = models.BooleanField(default=False)
+	items = models.ManyToManyField(OrderItem, on_delete=models.SET_NULL)
+	quantity = models.IntegerField(default=1)
+	price = models.DecimalField(decimal_places=2, default=0.00)
+	date_ordered = models.DateTimeField(auto_now=True)
+
+	def get_cart_items(self):
+		return self.items.all()
+
+	def get_cart_total(self):
+		return sum([item.product.price for item in self.items.all()])
+
+	def __str__(self):
+		return '{0} - {1}'.format(self.owner, self.ref_code)
