c076a6211b1cf20900347915945cb172c9ab647b
Ayobamidele <ayobamideleewetuga@gmail.com>
2021-10-19 16:29:42 +0100

    linked all models together

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from shop.models import Shop
 
 # Create your models here.
 class User(AbstractUser):
@@ -32,6 +33,7 @@ class Vendor(models.Model):
 			)
 	firstname = models.CharField(max_length=200, null=False)
 	lastname = models.CharField(max_length=200, null=False)
+	shopname = models.OneToOneField(Shop)
 	user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
 	date_created = models.DateTimeField(auto_now_add=True, null=True)
 	username = models.CharField(max_length=200, null=False)
