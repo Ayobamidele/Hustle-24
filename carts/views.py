9f613dcd5518f3a85069f642d1c21d294f3e6bf6
Ayobamidele <ayobamideleewetuga@gmail.com>
2021-11-23 10:38:23 +0100

    Added bootstrap adnd icons

--- carts/views.py
+++ carts/views.py
@@ -1,23 +1,18 @@
-from django.shortcuts import render
-from django.shortcuts import render
-from .models import *
-from accounts.models import *
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import get_user_model
 from django.shortcuts import get_object_or_404
-
+from django.forms import inlineformset_factory
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.forms import inlineformset_factory
-from django.contrib.auth.forms import UserCreationForm
-
-from django.contrib.auth import authenticate, login, logout
-
 from django.contrib import messages
-
-from django.contrib.auth.decorators import login_required
-
-
-# Create your views here.
-from django.contrib.auth import get_user_model
+from django.shortcuts import render
 from .decorators import *
+from .models import *
 
 # Create your views here.
+
+def cart(request):
+	context = {}
+	return render(request, 'carts/cart.html', context)
