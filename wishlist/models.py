e8b0400844f454da2809e10f1f1fa6e6d8c97f00
unknown <ayobamideleewetuga@gmail.com>
2022-03-10 09:39:04 +0100

    Add field customer to wishlist.

--- wishlist/models.py
+++ wishlist/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from shop.models import Product
+from accounts.models import Customer
 
 # Create your models here.
 class WishList(models.Model):
+	customer = models.ForeignKey(Customer, on_delete=models.CASCADE, null=True, related_name="Customer")
 	product =  models.ForeignKey(Product, on_delete=models.CASCADE, null=True)
 	initial_Price = models.DecimalField(decimal_places=2,max_digits=100)
 	initial_quantity = models.IntegerField(default=0)
