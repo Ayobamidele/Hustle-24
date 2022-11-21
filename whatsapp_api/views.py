535ae13a06681d60eec4cb7b4a610b9db3e03f10
Ayobamidele <ayobamideleewetuga@gmail.com>
2022-11-21 13:39:28 +0100

    Testing the webhook view.

--- whatsapp_api/views.py
+++ whatsapp_api/views.py
@@ -1,18 +1,24 @@
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
 
 
+
+@csrf_exempt
 def webhook(request):
 	token = "Testing12345"
 	if request.method == 'POST':
-		if request.GET.get("hub.mode") == "subscribe" and request.GET.get("hub.challenge"):
-			if not request.GET.get("hub.verify_token")== token:
-				return HttpResponse("Verification token missmatch", status=403)
-			return HttpResponse(request.GET['hub.challenge'], status=200)
-		print(request.GET)
-		return HttpResponse("Hello world", status=200)
+		print("Here i am", dir(request.POST), request.GET)
+		if request.GET.get("hub.mode") == "subscribe":
+			if request.GET.get("hub.verify_token") != token:
+				return HttpResponse("Verification token missmatch")
+			return HttpResponse(request.GET.get("hub.challenge"), content_type="text/plain")
+		# return HttpResponse("Hello world", status=200)
 		# return HttpResponse(request.body, status=200)
 	elif request.method == 'GET':
-		print(request.POST)
-		return HttpResponse(request.body, status=200)
+		print("Here i am", dir(request.POST), request.GET)
+		if request.POST.get("hub.mode") == "subscribe":
+			if request.POST.get("hub.verify_token") != token:
+				return HttpResponse("Verification token missmatch")
+			return HttpResponse(request.POST.get("hub.challenge"), content_type="text/plain")
 	
