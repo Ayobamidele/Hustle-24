ab024419a837072c3a3bc600cdf3db612ee513fe
Ayobamidele <ayobamideleewetuga@gmail.com>
2022-11-22 13:06:49 +0100

    Made changes to csrf in settings.

--- Hustle_24/settings.py
+++ Hustle_24/settings.py
@@ -26,8 +26,11 @@ SECRET_KEY = 'django-insecure-nam74v@zgf8=0=zgt!q7=7d&=#%jvugqpbidqv6kl#8foorape
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 # ALLOWED_HOSTS = ['a9ad-102-89-22-6.eu.ngrok.io']
-ALLOWED_HOSTS = [ '.ngrok.io', '127.0.0.1', '.telebit.io' , '.railway.app', '.loclx.io' ]
-
+ALLOWED_HOSTS = [ '.ngrok.io', '127.0.0.1', '.telebit.io' , '.railway.app' , '.loclx.io' , '*' ]
+CSRF_TRUSTED_ORIGINS = [
+    'https://hustle-24-production.up.railway.app',
+    '*'
+]
 # ALLOWED_HOSTS = ['a9ad-102-89-22-6.eu.ngrok.io']
 
 # Application definition
